9c3a710c177395b1faa02a5d37d73574fabd088e
Aleksandre Kapanadze <aleksandre.kapanadze@outlook.com>
2024-02-10 23:27:37 +0400

    Pluralsight. ASP.NET Core 6 Web API Fundamentals. Part 7.

---
+++ Pluralsight/KevinDockx/CityInfo/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,16 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
+        Task<bool> CityExistsAsync(int cityId);
+        Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInteresId);
+        Task AddPointOfInterestForCityAsync(int cityId, PointOfInterest pointOfInteres);
+        void DeletePointOfInterest(PointOfInterest pointOfInterest);
+        Task<bool> SaveChangesAsync();
+    }
+}
